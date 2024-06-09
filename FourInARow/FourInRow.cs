70e3af5863d4f528bc7486f7b4af93e631c553c9
DavidIbanezMoya <davidibanezmoya@gmail.com>
2024-06-09 10:23:16 +0200

    Add project files.

---
+++ FourInARow/FourInRow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FourInARow
+{
+    class FourInRow
+    {
+           static void Main(string[] args)
+        {
+            Console.Write("FourInRow test");
+        }
+    }
+}
---
+++ FourInARow/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourInARow
+{
+    //Class that is gonna store the info of the player
+    class Player
+    {
+    }
+}
